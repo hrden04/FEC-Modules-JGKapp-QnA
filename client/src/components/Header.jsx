23405e2119525a0628d1f87c1712b4053ac80a32
Joel Kapp <joel.g.kapp@gmail.com>
2020-06-24 12:34:20 -0600

    Fix linting error

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-const Header = () => {
-  return (
-    <div className="title">
-      <hr />
-      <h2>Customer questions & answers</h2>
-    </div>
-  );
-};
+const Header = () => (
+  <div className="title">
+    <hr />
+    <h2>Customer questions & answers</h2>
+  </div>
+);
 
 export default Header;
