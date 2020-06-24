840cd14c9cdcd6e58f60e0efef053aa77a4706d3
Joel Kapp <joel.g.kapp@gmail.com>
2020-06-24 14:51:25 -0600

    Implement tests for Search component

---
+++ _tests_/Search.test.jsx
@@ -0,0 +1,19 @@
+/* eslint-env jest */
+import React from 'react';
+import { mount } from 'enzyme';
+import Search from '../client/src/components/Search.jsx';
+
+describe('Search Component', () => {
+  test('should render the search component on the screen', () => {
+    const wrapper = mount(<Search />);
+    expect(wrapper).toExist();
+  });
+
+  test('should allow text input', () => {
+    const wrapper = mount(<Search />);
+    const testVal = 'test';
+    wrapper.find('.search-input').simulate('change', { target: { value: testVal } });
+    console.log(wrapper.debug());
+    expect(wrapper.find('.search-input').instance().value).toBe(testVal);
+  });
+});
